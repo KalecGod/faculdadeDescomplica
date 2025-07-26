dbfa38ae3b6a6afa4a2e69f7b2ade88fe0aaf39a
KalecGod <adalbertomc123@gmail.com>
2025-07-26 18:15:15 -0300

    weatherDecision

---
+++ weatherDecision.js
@@ -0,0 +1,15 @@
+// Variavel que representa a previsão do tempo
+
+// Pode ser "rainy" (Chuvoso), "sunny" (Ensolarado) ou "cloudy" (Nublado)
+
+let weatherForecast = "rainy";  
+
+//Analogia com a vida real : Decidindo se devemos levar um guarda-chuva
+
+if(weatherForecast === "rainy") {
+    console.log("Leve um guarda-chuva.");
+}   else if(weatherForecast === "sunny") {
+    console.log("Use óculos de sol.");
+} else
+    console.log("Use um casaco leve."); 
+    
