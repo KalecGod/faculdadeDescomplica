f3704afe42d83847c6b46fbc0ecc82234aeada6e
KalecGod <adalbertomc123@gmail.com>
2025-07-26 11:50:24 -0300

    OperaçãoAtribuição

---
+++ operadoresAtribuicao.js
@@ -0,0 +1,39 @@
+// Exemplos de Operadores de Atribuição em JavaScript
+
+// Operador de Atribuição basico (=)
+
+let x = 10; // Atribui o valor 10 à variável x
+console.log(`valor de x :${x}`);
+
+//Operador de Atribuição adição (+=)
+x +=5;
+console.log(`valor de x após adição: ${x}`); // Atribui o valor de x + x à variável x
+
+// Operador de Atribuição subtração (-=)
+
+x-=3;
+console.log(`valor de x após subtração: ${x}`); // Atribui o valor de x - 3 à variável x    
+
+// Operador de Atribuição multiplicação (*=)
+
+x *= 2;
+console.log(`valor de x após multiplicação: ${x}`); // Atribui o valor de x * 2 à variável x
+
+// Operador de Atribuição divisão (/=)
+
+x*= 4;
+console.log(`valor de x após divisão: ${x}`); // Atribui o valor de x / 4 à variável x
+
+
+// Operador de Atribuição módulo (%=)
+
+x %= 30;
+console.log(`valor de x após módulo: ${x}`); // Atribui o valor
+
+// Operador de Atribuição exponenciação (**=)
+
+x **= 2;
+console.log(`valor de x após exponenciação: ${x}`); // Atribui o valor de x ** 2 à variável x
+
+
+
