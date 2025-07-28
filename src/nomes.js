5d03b28ed9036e7ff24594618a24c0ea5208a44f
KalecGod <adalbertomc123@gmail.com>
2025-07-28 13:05:17 -0300

    toLower_toUpper

---
+++ src/nomes.js
@@ -0,0 +1,24 @@
+//Definindo uma string de exemplo
+
+const nome = "Rebeka";
+
+//Usando o toLowerCasa();
+const nomeMinusculo = nome.toLowerCase();
+console.log(nomeMinusculo)
+
+//Usando o toUpperCasa();
+
+const nomeMaiuculo = nome.toUpperCase();
+console.log(nomeMaiuculo)
+
+// Simbolos
+
+const simbolos = "Rebeka Gabriely, 20 Anos";
+
+let simbolosL = simbolos.toLowerCase();
+
+console.log(simbolosL);
+
+let simbolosU = simbolos.toUpperCase();
+
+console.log(simbolosU);
