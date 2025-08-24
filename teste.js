65355beafc6fb3c1c72aa3a052802a8b70675395
KalecGod <adalbertomc123@gmail.com>
2025-08-24 16:33:11 -0300

    add

--- teste.js
+++ teste.js
@@ -1,1 +1,28 @@
-<!DOCTYPE html>
+
+let minhaPromessa = new Promise((resolve,reject)=>{
+
+    let sucesso = true;
+
+    if(sucesso){
+        resolve("Sucesso")
+    }else{
+
+        reject("ERRO!")
+    }
+});
+
+
+minhaPromessa.then(result =>{
+
+    console.log(result);
+}).catch(error =>{
+    console.log(error);
+}).finally(()=>{
+    console.log("Operação Concluída")
+});
+
+
+
+
+
+
